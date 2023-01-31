7eb7ebeb23ec25853306c13d17d407ab97bf7764
Pyer23 <channelpla@gmail.com>
2023-01-31 14:55:25 +0100

    corretto pagina adotta
    corretto pagina adotta nel caso in cui non ci siano animali disponibili per l'adozione

--- php/adotta.php
+++ php/adotta.php
@@ -6,6 +6,9 @@ require_once ('card.php');
 
 $pagina=file_get_contents("../html/adotta.html");
 
+$cani = '<p>In questo momento non ci sono cani pronti per essere adottati. Riprova a controllare se ci sono nuovi cani più tardi.</p>';
+$gatti = '<p>In questo momento non ci sono gatti pronti per essere adottati. Riprova a controllare se ci sono nuovi gatti più tardi.</p>';
+
 if (isset($_SESSION['session_user'])) {
     $session_user = htmlspecialchars($_SESSION['session_user']);
     if($session_user=='utente'){
@@ -18,22 +21,26 @@ if (isset($_SESSION['session_user'])) {
         }
         $risultato_info_cane = $connessione->interrogaDB($query_info_cane);
         $risultato_info_gatto = $connessione->interrogaDB($query_info_gatto);
-        $listCard_cani = "";
-        $listCard_gatti = "";
+        $listCard_cani = '';
+        $listCard_gatti = '';
         if ($risultato_info_cane) {
             foreach ($risultato_info_cane as $ris) {
                 $card = new Card($ris);
                 $listCard_cani .= $card->makeCard();
             }
+        } else {
+            $listCard_cani = $cani;
         }
         if ($risultato_info_gatto) {
             foreach ($risultato_info_gatto as $ris) {
                 $card = new Card($ris);
                 $listCard_gatti .= $card->makeCard();
             }
+        } else {
+            $listCard_gatti = $gatti;
         }
         $animali = array($listCard_cani, $listCard_gatti);
-        $liste = array('%listaCani%', '%listaGatti%');
+        $liste = array('%listaCani%','%listaGatti%');
         $pagina = str_replace($liste, $animali, $pagina);
     
         echo $pagina;
@@ -57,14 +64,19 @@ if (isset($_SESSION['session_user'])) {
                 $listCard_cani .= $card->makeCard_admin();
             }
         }
+        else {
+            $listCard_cani = $cani;
+        }
         if ($risultato_info_gatto) {
             foreach ($risultato_info_gatto as $ris) {
                 $card = new Card($ris);
                 $listCard_gatti .= $card->makeCard_admin();
             }
+        } else {
+            $listCard_gatti = $gatti;
         }
         $animali = array($listCard_cani, $listCard_gatti);
-        $liste = array('%listaCani%', '%listaGatti%');
+        $liste = array('%listaCani%','%listaGatti%');
         $pagina = str_replace($liste, $animali, $pagina);
 
         echo $pagina;
@@ -89,14 +101,19 @@ if (isset($_SESSION['session_user'])) {
             $listCard_cani .= $card->makeCard_register();
         }
     }
+    else {
+        $listCard_cani = $cani;
+    }
     if ($risultato_info_gatto) {
         foreach ($risultato_info_gatto as $ris) {
             $card = new Card($ris);
             $listCard_gatti .= $card->makeCard_register();
         }
+    }  else {
+        $listCard_gatti = $gatti;
     }
     $animali = array($listCard_cani, $listCard_gatti);
-    $liste = array('%listaCani%', '%listaGatti%');
+    $liste = array('%listaCani%','%listaGatti%');
     $pagina = str_replace($liste, $animali, $pagina);
 
     echo $pagina;
