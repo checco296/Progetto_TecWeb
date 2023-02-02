eeb168c819ac414535b78611765e4386a07179f4
checco296 <francesco.ferraioli@studenti.unipd.it>
2023-02-02 23:12:41 +0100

    rimozione file superflui

--- DB/progetto_tecweb_utenti.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `users` (
- `id` int(11) NOT NULL AUTO_INCREMENT,
- `username` varchar(30) NOT NULL,
- `email` varchar(30) NOT NULL,
- `password` varchar(40) NOT NULL,
- `livello` varchar(20) NOT NULL,
- PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=39 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
